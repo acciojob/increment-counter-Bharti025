af86ef0c903c3916d692824b5a6415e98525e947
Bharti <110337668+Bharti025@users.noreply.github.com>
2025-08-18 23:40:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let button=document.getElementById("incrementBtn");
+let p=document.getElementById("counter");
+function incrementfunc(){
+	let n=parseInt(p.textContent);
+	alert(`The value is ${n}`);
+	p.textContent=n+1;
+}
+
+button.addEventListener("click",incrementfunc);
